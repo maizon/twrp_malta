6259e13e469e58423f53738559c261db5521383a
maizon <maizondroidplay@gmail.com>
2021-12-04 12:37:35 -0300

    test

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_malta.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_malta-user \
+    omni_malta-userdebug \
+    omni_malta-eng
